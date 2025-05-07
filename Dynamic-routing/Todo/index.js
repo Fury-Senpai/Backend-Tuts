460d4635f3dc1b0f468a35a19aa6f809533e4588
Fury-Senpai <harbola03nitish@gmail.com>
2025-05-08 00:14:13 +0530

    initial commit

--- Dynamic-routing/Todo/index.js
+++ Dynamic-routing/Todo/index.js
@@ -1,16 +1,25 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const fs = require('path');
+const fs = require('fs');
 
 app.set('view engine' , 'ejs');
 app.use(express.json());
 app.use(express.static(path.join(__dirname,"public")));
 app.use(express.urlencoded({extended:true}));
 
-app.get('/' , (req,res)=>{
-    re
-});
+app.get('/',(req,res)=>{
+    fs.readdir('./files',(err,files)=>{
+        res.render('index',{files:files});
+    })
+})
+    
+app.get('/files' , (req,res)=>{
+    fs.readdir('./files',(err,files)=>{
+        if (err) throw err;
+        res.json(files);
+    })
+})
 
 app.post('/create' , (req,res)=>{
     const fileName = `${req.body.title}`
