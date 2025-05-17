ba4c39ca6c8ad31d9569f4bcadeff9b8a171964d
Fury-Senpai <harbola03nitish@gmail.com>
2025-05-17 16:50:04 +0530

    commit

--- Dynamic-routing/Todo/index.js
+++ Dynamic-routing/Todo/index.js
@@ -14,22 +14,59 @@ app.get('/',(req,res)=>{
     })
 })
     
-app.get('/files' , (req,res)=>{
-    fs.readdir('./files',(err,files)=>{
+app.get('/files/:filename' , (req,res)=>{
+    fs.readFile(`./files/${req.params.filename}`, "utf-8",(err,filedata)=>{
         if (err) throw err;
-        res.json(files);
+        res.render('show' , {filename: req.params.filename , filedata: filedata});
+    })
+})
+
+app.get('/edit/:filename' , (req,res)=>{
+    fs.readFile(`./files/${req.params.filename}`, "utf-8",(err,filedata)=>{
+        if (err) throw err;
+        res.render('edit' , {filename: req.params.filename , filedata: filedata});
     })
 })
 
 app.post('/create' , (req,res)=>{
-    const fileName = `${req.body.title}`
+    const fileName = `${req.body.title}.txt`
     const pathName = path.join(__dirname , 'files' , fileName);
     const fileContent = `${req.body.title}\n${req.body.details}`;
 
-    fs.writeFile(fileName , fileContent , (err)=>{
+    fs.writeFile(pathName , fileContent , (err)=>{
         if(err) throw err;
         res.redirect('/')
     })
 })
 
+app.post('/edit' , (req,res)=>{
+    const oldTitle = req.body.oldTitle.trim() + '.txt';
+    const newTitle = req.body.newTitle.trim() + '.txt';
+    const newDetail = req.body.newDetail.trim(); 
+
+    const oldPath = path.join(__dirname , 'files' , oldTitle);
+    const newPath = path.join(__dirname , 'files' , newTitle);
+    const newFileContent = `${newDetail}`
+
+    const renameAndWrite = () => {
+        fs.writeFile(newPath, newFileContent, (err) => {
+            if (err) return res.status(500).send("Failed to write content");
+            res.redirect('/');
+        });
+    };
+    
+    if (newTitle !== oldTitle) {
+        fs.rename(oldPath, newPath, (err) => {
+            if (err) return res.status(500).send("Rename failed");
+            renameAndWrite();
+        });
+    } else {
+        // Title not changed, just overwrite
+        fs.writeFile(oldPath, newFileContent, (err) => {
+            if (err) return res.status(500).send("Write failed");
+            res.redirect('/');
+        });
+    }
+});
+    
 app.listen(3000);
